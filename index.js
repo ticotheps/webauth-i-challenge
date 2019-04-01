a26c26ac4e00b27990febaed062422bf789ad1c8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 18:10:00 -0400

    Modifies the restricted() middleware function to reflect the addition of the express-session module

--- index.js
+++ index.js
@@ -78,30 +78,40 @@ function only(username) {
     };
 }
 
+//-----------NEW MIDDLEWARE; after addition of express-sessions--------------
+function restricted(req, res, next) {
 
+    if (req.session && req.session.username) {
+      next();
+    } else {
+      res.status(401).json({ message: 'You shall not pass!' });
+    }
+  }
+
+// ----------OLD MIDDLEWARE; before addition of express-sessions-------------
 // middleware that checks the credentials of the user (in the headers of the GET request) 
 // before allowing access to the list of 'users'
-function restricted(req, res, next) {
-    const { username, password } = req.headers;
+// function restricted(req, res, next) {
+//     const { username, password } = req.headers;
 
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                // check the password against the database
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    next();
-                } else {
-                    res.status(401).json({ message: 'Invalid Credentials' });
-                }
-            })
-            .catch(error => {
-                res.status(500).json({ message: 'You shall not pass!' });
-            });
-    } else {
-        res.status(400).json({ message: 'No credentials provided' });
-    }; 
-}
+//     if (username && password) {
+//         Users.findBy({ username })
+//             .first()
+//             .then(user => {
+//                 // check the password against the database
+//                 if (user && bcrypt.compareSync(password, user.password)) {
+//                     next();
+//                 } else {
+//                     res.status(401).json({ message: 'Invalid Credentials' });
+//                 }
+//             })
+//             .catch(error => {
+//                 res.status(500).json({ message: 'You shall not pass!' });
+//             });
+//     } else {
+//         res.status(400).json({ message: 'No credentials provided' });
+//     }; 
+// }
 
 // allows a user to gain access to the list of 'users' in the db
 server.get('/api/users', restricted, only('tico'), (req, res) => {
