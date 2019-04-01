c4c92e5330749cbfc4c225e12fa612ee63e57ef3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 11:36:23 -0400

    Creates an index.js file to store our server and request handlers in

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send("Yay! My server is working!");
+  });
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`\n** Web-Auth-I Challenge API Running on port ${port} **\n`));
