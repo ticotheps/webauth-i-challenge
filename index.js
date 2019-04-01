2a46d3683d393f9439abca3a7480096c4a4bbc64
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 19:33:05 -0400

    Curries the 'KnexSessionStore(session);' invokation in index.js right on to the end of the value of the 'KnexSessionStore' variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
