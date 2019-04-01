772f5aef8763b2d9344d5b7fddb71b7028fe50ca
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 13:03:08 -0400

    Creates a POST request handler that uses the bcrypt.js library to hash the password that is passed into req.body

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ server.use(cors());
 
 server.get('/', (req, res) => {
     res.send("Yay! My server is working!");
-  });
+});
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    /// hash the password
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+        .then(savedUser => {
+            res.status(201).json(savedUser);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n** Web-Auth-I Challenge API Running on port ${port} **\n`));
