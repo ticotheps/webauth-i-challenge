46abb8bb777d1406250247e54da97748564bc383
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 11:37:48 -0400

    Defines a variable 'server' in index.js to create an instantiation of an expres server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
+const server = express();
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
