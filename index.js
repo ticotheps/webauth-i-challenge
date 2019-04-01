38a6352eb034d468acba04718498606a30174066
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-01 13:38:56 -0400

    Creates a GET request handler and a restricted() middleware function that will allow only users with proper credentials to have access to the '/api/users' endpoint

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+
 server.get('/', (req, res) => {
     res.send("Yay! My server is working!");
 });
 
+// allows a user to add a username and hashed password to the database
 server.post('/api/register', (req, res) => {
     let user = req.body;
 
@@ -28,6 +30,7 @@ server.post('/api/register', (req, res) => {
             res.status(500).json(error);
         })
 });
+
 
 server.post('/api/login', (req, res) => {
     let { username, password } = req.body;
@@ -46,6 +49,43 @@ server.post('/api/login', (req, res) => {
             res.status(500).json(error);
         })
 });
+
+// restrticts access to the '/api/login' endpoint to only users 
+// that provide the right credentials in the heaaders
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+// middleware that 
+function restricted(req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                // check the password against the database
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Ran into an unexpected error' });
+            })
+        next();
+    } else {
+        res.status(401).json({ message: 'No credentials provided' });
+    }; 
+}
+
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n** Web-Auth-I Challenge API Running on port ${port} **\n`));
