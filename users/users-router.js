ef5962d9c2d97c30a16672ca60c9ebe5f5fb1b91
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-02 16:14:54 -0400

    Creates a new 'restricted-middleware.js' file inside the 'auth' folder to separate this middleware from the users-router.js file

--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Users = require('./users-model.js');
+const restricted = require('../auth/restricted-middleware.js');
 
 // this middleware only allows for specific users to have access to the list of 'users' in the db
 function only(username) {
@@ -12,41 +13,6 @@ function only(username) {
       }
     };
 }
-
-//-----------NEW MIDDLEWARE; after addition of express-sessions--------------
-function restricted(req, res, next) {
-
-    if (req.session && req.session.username) {
-      next();
-    } else {
-      res.status(401).json({ message: 'You shall not pass!' });
-    }
-  }
-
-// ----------OLD MIDDLEWARE; before addition of express-sessions-------------
-// middleware that checks the credentials of the user (in the headers of the GET request) 
-// before allowing access to the list of 'users'
-// function restricted(req, res, next) {
-//     const { username, password } = req.headers;
-
-//     if (username && password) {
-//         Users.findBy({ username })
-//             .first()
-//             .then(user => {
-//                 // check the password against the database
-//                 if (user && bcrypt.compareSync(password, user.password)) {
-//                     next();
-//                 } else {
-//                     res.status(401).json({ message: 'Invalid Credentials' });
-//                 }
-//             })
-//             .catch(error => {
-//                 res.status(500).json({ message: 'You shall not pass!' });
-//             });
-//     } else {
-//         res.status(400).json({ message: 'No credentials provided' });
-//     }; 
-// }
 
 // allows a user to gain access to the list of 'users' in the db
 router.get('/', restricted, only('tico'), (req, res) => {
