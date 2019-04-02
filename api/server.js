bafa99458003e43ae58b8ff5a5600d3e4795c746
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-02 15:36:21 -0400

    Adds commented code to server.js file and also moves the 'httpOnly' key/value pair into the 'cookie' object on 'sessionConfig'

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const KnexSessionStore = require('connect-session-knex')(session);
 
 const db = require('../database/dbConfig.js');
 const Users = require('../users/users-model.js');
+
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
@@ -15,19 +16,21 @@ const sessionConfig = {
     name: 'doodie',
     secret: 'yankee doodle went to town',
     cookie: {
-        maxAge: 1000 * 60 * 30, // equals 30 minutes
-        secure: false,
+        maxAge: 1000 * 60 * 30, // equals 30 minutes (in milliseconds)
+        secure: false, // Use cookie over https? 'false' during development, 'true' during production;
+        httpOnly: true, // Can JS access the cookie on the browser?
     },
-    httpOnly: true,
-    resave: false,
+    resave: false, // avoid recreating unchanged session data
     saveUninitialized: false,
+    // "false" = does not force users to accept cookies, follows FDPR compliance
+    // "true" = forces user to accept cookies
 
     store: new KnexSessionStore({
         knex: db,
         tablename: 'sessions',
         sidfieldname: 'sid',
         createtable: true,
-        clearInterval: 1000 * 60 * 60, // in ms
+        clearInterval: 1000 * 60 * 60, // deletes expired session every 60 minutes (in ms)
     }),
 };
 
