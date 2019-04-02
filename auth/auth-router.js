cfc6615ca0d366b0dbbb1718c9fee7b7594b8251
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-02 17:33:25 -0400

    Initial commit on 'web-auth-guided-iii' branch removes any code related to 'sessions'

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -13,9 +13,6 @@ router.post('/register', (req, res) => {
 
     Users.add(user)
         .then(saved => {
-            // HERE is where we can PERSIST the session data for the user
-            req.session.username = saved;
-
             res.status(201).json(saved);
         })
         .catch(error => {
@@ -33,9 +30,6 @@ router.post('/login', (req, res) => {
         .then(user => {
             // check the password guess against the database
             if (user && bcrypt.compareSync(password, user.password)) {
-                // HERE is where we want to save cookie data regarding the session
-                req.session.username = user.username;
-
                 res.status(200).json({ messsage: `Welcome ${user.username}! You have received a cookie!` });
             } else {
                 res.status(401).json({ message: 'You shall not pass!' });
@@ -44,21 +38,6 @@ router.post('/login', (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         })
-});
-
-// allows the user to log out of the current session
-router.get('/logout', (req, res) => {
-    if (req.session) {
-        req.session.destroy(err => {
-            if (err) {
-                res.status(500).json({ message: "You can checkout if you'd like, but you can never leave!"});
-            } else {
-                res.status(200).json({ message: "Goodbye! Thanks for playing!"});
-            }
-        })
-    } else {
-        res.status(200).json({ message: "Goodbye! Thanks for playing!" });
-    }
 });
 
 module.exports = router;
--- auth/only-middleware.js
+++ auth/only-middleware.js
@@ -1,4 +1,3 @@
-// this middleware only allows for specific users to have access to the list of 'users' in the db
 function only(username) {
     return function(req, res, next) {
       if (req.headers.username === username) {
