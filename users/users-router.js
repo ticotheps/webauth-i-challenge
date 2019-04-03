63b68e63dfc3ebb57eede791074dc80dc90cce18
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-03 11:03:19 -0400

    Comments out 'tico-only' version of GET request to '/' endpoint in users-router.js file

--- users/users-router.js
+++ users/users-router.js
@@ -4,8 +4,19 @@ const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 const only = require('../auth/only-middleware.js');
 
-// allows a user to gain access to the list of 'users' in the db
-router.get('/', restricted, only('tico'), (req, res) => {
+// allows only 'tico' to gain access to the list of 'users' in the db
+// router.get('/', restricted, only('tico'), (req, res) => {
+//     Users.find()
+//         .then(users => {
+//             res.json(users);
+//         })
+//         .catch(error => {
+//             res.send(error);
+//         });
+// });
+
+// allows only authorized users to gain access to the list of 'users' in the db
+router.get('/', restricted, (req, res) => {
     Users.find()
         .then(users => {
             res.json(users);
